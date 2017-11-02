b110cb1d9d9a5a9288d492305160149e5fa0c35e
zhuyahui <zhuyahui@szmtc.com.cn>
2017-11-02 11:49:44 +0800

    Initial commit

---
+++ app/src/main/java/com/example/zhyh/myapplication/MainActivity.java
@@ -0,0 +1,99 @@
+package com.example.zhyh.myapplication;
+
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import com.example.zhyh.util.DensityUtil;
+import com.example.zhyh.util.EncodingUtils;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        create("aaabaa","12345566");
+    }
+    /**
+     * 创建二维码并将图片保存在本地
+     */
+    private void create(String contentStr, String phnoeNumber) {
+        int width = DensityUtil.dip2px(this, 200);
+        Bitmap bitmap = EncodingUtils.createQRCode(contentStr, width, width,
+                BitmapFactory.decodeResource(getResources(), R.drawable.ic_launcher_background));
+        saveBitmap(bitmap, phnoeNumber);
+    }
+
+    /**
+     * 将Bitmap保存在本地
+     *
+     * @param bitmap
+     */
+    public void saveBitmap(Bitmap bitmap, String phoneNumber) {
+        // 首先保存图片
+        File appDir = new File(Environment.getExternalStorageDirectory(),
+                "mtc_image");
+        if (!appDir.exists()) {
+            appDir.mkdir();
+        }
+        String fileName = "mtc_image" + "_" + phoneNumber + ".jpg";
+        File file = new File(appDir, fileName);
+
+    }
+}
